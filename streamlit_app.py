1b484e8e8264bb535535abb8cebe2766f1af2cc6
$H0 <meishao2002@msn.com>
2024-09-15 12:59:55 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
+from auth import auth
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Home")
+
+supabase = auth()
+if supabase is None:
+    st.stop()
+
+user = supabase.auth.get_user()
+user_name = user.user.user_metadata['name']
+
+st.write(f"Hey {user_name}, welcome to your streamlit app!")
+
+
+# Sign out
+if st.button("Sign out"):
+    supabase.auth.sign_out()
+    st.success("Signed out successfully")
+    st.rerun()
